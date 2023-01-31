12170f58d31d4f942e05bfd533a8b26d389c123f
Ravi Xavier <ravisxavier@gmail.com>
2023-01-30 22:07:51 -0300

    JS- Day creation and day creation check

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const form = document.querySelector("#form-habits");
-
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-nlwSetup.addDay("01/02");
+button.addEventListener("click", add);
 
-nlwSetup.dayExists("01/02");
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, 5);
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert("Dia já registrado!");
+    return;
+  }
+
+  alert("Dia registrado com sucesso🎇");
+  nlwSetup.addDay(today);
+}
+
+// const data = {
+//   organize: ["01-01", "01-02", "01-03"],
+//   coffee: ["01-01"],
+//   study: ["01-01"],
+// };
+
+// nlwSetup.setData(data);
+// nlwSetup.load();
