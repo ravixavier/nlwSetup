2c2662a1fbde65ceac792ddd2a64d3c4f56fdf21
Ravi Xavier <ravisxavier@gmail.com>
2023-02-02 00:10:38 -0300

    js function saving in local storage(JSON.stringify/parse(nlwSetup.data)

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const nlwSetup = new NLWSetup(form);
 const button = document.querySelector("header button");
 
 button.addEventListener("click", add);
+form.addEventListener("change", save);
 
 function add() {
   const today = new Date().toLocaleDateString("pt-br").slice(0, 5);
@@ -17,11 +18,12 @@ function add() {
   nlwSetup.addDay(today);
 }
 
-// const data = {
-//   organize: ["01-01", "01-02", "01-03"],
-//   coffee: ["01-01"],
-//   study: ["01-01"],
-// };
+function save() {
+  console.log("Saving...");
+  localStorage.setItem("NLWsetup@habits", JSON.stringify(nlwSetup.data));
+}
 
-// nlwSetup.setData(data);
-// nlwSetup.load();
+const data = JSON.parse(localStorage.getItem("NLWsetup@habits")) || {};
+
+nlwSetup.setData(data);
+nlwSetup.load();
